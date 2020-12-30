58d30259d2d2e76e49c2bfcbb1a1d003f8f54eee
IvanKopych <ivan.kopych@gmail.com>
2020-12-30 14:11:15 +0200

    Final build
    Did it

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -8,14 +8,35 @@
 
 import UIKit
 
+
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let text = K.appName
+        var charIndex = 0.0
 
-       
+        titleLabel.text = ""
+        for char in text {
+            Timer.scheduledTimer(withTimeInterval: charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(char)
+            }
+            charIndex += 0.2
+        }
+
+
     }
     
 
